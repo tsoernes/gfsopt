a3113f1814b7bcd82b7a1ee14a6c5a8c5144662f
torstein <t.soernes@gmail.com>
2018-06-18 01:03:46 +0200

    Try rename

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'main'
+master_doc = 'index'
 
 # General information about the project.
 project = 'gfsopt'
